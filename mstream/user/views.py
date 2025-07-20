3b481dac1eeff3b5ccafedb222e68b7a8751614e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-20 22:58:28 +0530

    Views File Structure Defined

--- mstream/user/views.py
+++ mstream/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
+@api_view(['POST'])
+def create_account(request):
+    pass
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def delete_user(request):
+    pass
