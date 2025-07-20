e572a8096bb3013189145a794fdf50d86616b6a9
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-20 23:44:31 +0530

    Serializers File Written Completly

--- mstream/user/serializers.py
+++ mstream/user/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework.serializers import ModelSerializer,CharField
+from rest_framework.serializers import ModelSerializer,CharField,ValidationError
 class UserSerializer(ModelSerializer):
     confirm_password = CharField(write_only=True)
     class Meta:
@@ -10,3 +10,33 @@ class UserSerializer(ModelSerializer):
             'password',
             'confirm_password'
         ]
+    def validate(self, attrs):
+        if User.objects.filter(email=attrs['email']).exists():
+            raise ValidationError(
+                {
+                    "Error":"Account Associated With This Email Already Existed"
+                }
+            )
+        elif User.objects.filter(username=attrs['username']).exists():
+            raise ValidationError(
+                {
+                    "Error":"Account Associated With This Username Already Existed"
+                }
+            )
+        elif attrs['password'] == attrs['confirm_password']:
+            raise ValidationError(
+                {
+                    "Error":"Passwords Do Not Match"
+                }
+            )
+        else:
+            return attrs
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
