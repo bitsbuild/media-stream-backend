1140a7771acf091062280905c15836860e552427
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-20 23:54:17 +0530

    Create User Completed, Views Completed User App Completed, Testing Pending

--- mstream/user/views.py
+++ mstream/user/views.py
@@ -3,9 +3,25 @@ from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
+from user.serializers import UserSerializer
 @api_view(['POST'])
 def create_user(request):
-    pass
+    try:
+        srlzr =  UserSerializer(data=request.data)
+        srlzr.is_valid(raise_exception=True)
+        srlzr.save()
+        return Response(
+            {
+                "Status":"Account Creation Successful"
+            },status=HTTP_200_OK
+        )
+    except Exception as e:
+        return Response(
+            {
+                "Status":"Error",
+                "Error":str(e)
+            },status=HTTP_400_BAD_REQUEST
+        )
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def delete_user(request):
