2d4ef7b0af3500961317f18a2072df768b1a6d90
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-20 23:48:33 +0530

    Account Deletion Completed

--- mstream/user/views.py
+++ mstream/user/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
 @api_view(['POST'])
 def create_user(request):
     pass
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def delete_user(request):
-    pass
+    try:
+        request.user.delete()
+        return Response(
+            {
+                "Status":"Account Deleted Successfully"
+            },status=HTTP_200_OK
+        )
+    except Exception as e:
+        return Response(
+            {
+                "Status":"Error",
+                "Error":str(e)
+            },status=HTTP_400_BAD_REQUEST
+        )
