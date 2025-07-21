c296bbc59535976e12b5f61f7c8ee13bc39698a5
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-21 18:40:28 +0530

    Models For The App Defined And Added To The Admin Panel

--- mstream/app/admin.py
+++ mstream/app/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from app.models import (
+ ContentGenre,
+ Artist,
+ Content,
+ ArtistContentRelation,
+ Reviews,
+ SubscriptionModels,
+ SubscriptionMapping,
+)
+admin.site.register(ContentGenre)
+admin.site.register(Artist)
+admin.site.register(Content)
+admin.site.register(ArtistContentRelation)
+admin.site.register(Reviews)
+admin.site.register(SubscriptionModels)
+admin.site.register(SubscriptionMapping)
--- mstream/app/models.py
+++ mstream/app/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+class ContentGenre(models.Model):
+    pass
+class Artist(models.Model):
+    pass
+class Content(models.Model):
+    pass
+class ArtistContentRelation(models.Model):
+    pass
+class Reviews(models.Model):
+    pass
+class SubscriptionModels(models.Model):
+    pass
+class SubscriptionMapping(models.Model):
+    pass
