2aa38a0c176bdefcd69fac44c6d278f49c8245e6
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-22 10:33:01 +0530

    App Level Urls Registered To Project

--- mstream/mstream/urls.py
+++ mstream/mstream/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/',include('user.urls'))
+    path('user/',include('user.urls')),
+    path('api/',include('app.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
