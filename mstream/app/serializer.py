b22a38a7147b5f66bde2c855d83ced2ce61c5e08
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-23 11:11:52 +0530

    Serializer-1

--- mstream/app/serializer.py
+++ mstream/app/serializer.py
@@ -10,44 +10,46 @@ from app.models import (
     ContentMediaFile,
     ContentType
 )
-from rest_framework.serializers import ModelSerializer
-class ContentGenreSerializer(ModelSerializer):
-    class Meta:
-        model = ContentGenre
-        fields = '__all__'
-class ArtistSerializer(ModelSerializer):
-    class Meta:
-        model = Artist
-        fields = '__all__'
-class ContentSerializer(ModelSerializer):
+from rest_framework import serializers
+class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Content
         fields = '__all__'
-class ArtistContentRelationSerializer(ModelSerializer):
+class ContentGenreSerializer(serializers.ModelSerializer):
+    content = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = ContentGenre
+        fields = '__all__'
+class ArtistSerializer(serializers.ModelSerializer):
+    content = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = Artist
+        fields = '__all__'
+class ArtistContentRelationSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArtistContentRelation
         fields = '__all__'
-class ReviewsSerializer(ModelSerializer):
+class ReviewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reviews
         fields = '__all__'
-class SubscriptionModelsSerializer(ModelSerializer):
+class SubscriptionModelsSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubscriptionModels
         fields = '__all__'
-class SubscriptionMappingSerializer(ModelSerializer):
+class SubscriptionMappingSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubscriptionMapping
         fields = '__all__'
-class ParentalControlTagsSerializer(ModelSerializer):
+class ParentalControlTagsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParentalControlTags
         fields = '__all__'
-class ContentMediaFileSerializer(ModelSerializer):
+class ContentMediaFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContentMediaFile
         fields = '__all__'
-class ContentTypeSerializer(ModelSerializer):
+class ContentTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContentType
         fields = '__all__'
