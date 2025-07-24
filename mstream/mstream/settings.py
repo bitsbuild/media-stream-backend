6d047b11582a3cc03c8904bbdc1c3beeec2f308b
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-24 14:48:25 +0530

    Pagination Completed

--- mstream/mstream/settings.py
+++ mstream/mstream/settings.py
@@ -73,6 +73,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication'
     ],
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':20
 }
 MEDIA_URL = '/content/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"content")
